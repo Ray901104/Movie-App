6d6908d18ff5b9055103000e34285bd7d632a750
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-12 16:03:15 +0900

    GridCard 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../../Config";
 import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCards";
+import { Row } from "antd";
 
 function LandingPage() {
   const [Movies, setMovies] = useState([]);
@@ -13,7 +15,8 @@ function LandingPage() {
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
-        setMovies([response.results]);
+        setMovies([...response.results]);
+        // console.log(Movies);
         setMainMovieImage(response.results[0]);
       });
   }, []);
@@ -34,6 +37,22 @@ function LandingPage() {
         <h2>최신 영화 목록</h2>
         <hr />
         {/* Movie Grid Cards */}
+        <Row gutter={[16, 16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={
+                    movie.poster_path
+                      ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                      : null
+                  }
+                  movieId={movie.id}
+                  movieName={movie.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <button>더보기</button>
