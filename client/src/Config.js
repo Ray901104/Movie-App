99a692d84b7fe489a3adea6e6bd20b92d988ac0a
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-12 14:31:08 +0900

    랜딩페이지 - 메인이미지 가져오기

---
+++ client/src/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+
+//API URL
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "0fd55d55d9c991d012b605d96f33c588";
+//IMAGE URL
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,39 +1,43 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../../Config";
+import MainImage from "./Sections/MainImage";
 
 function LandingPage() {
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+
   useEffect(() => {
-    axios.get("/api/hello").then((response) => {
-      console.log(response.data);
-    });
-  });
+    const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
-  const navigate = useNavigate();
-
-  const onClickHandler = () => {
-    axios.get("/api/users/logout").then((response) => {
-      console.log(response.data);
-      if (response.data.success) {
-        navigate("/login");
-      } else {
-        alert("로그아웃 실패");
-      }
-    });
-  };
+    fetch(endPoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setMovies([response.results]);
+        setMainMovieImage(response.results[0]);
+      });
+  }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <h2>시작 페이지</h2>
-      <button onClick={onClickHandler}>로그아웃</button>
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* Main Image */}
+      {/* 렌더링이 먼저 되기 때문에 state에 MainMovieImage가 없는 상태, 이를 처리하기 위해
+          MainMovieImage가 있으면(&&)이를 처리하라는 코드를 작성해주어야 한다.*/}
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>최신 영화 목록</h2>
+        <hr />
+        {/* Movie Grid Cards */}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
     </div>
   );
 }
